f9cef6a4047eab48a674e5e848a32c8383f46c06
Tim Neutkens <tim@timneutkens.nl>
2026-02-24 20:50:20 +0100

    Node.js streams: First pass

--- packages/next/src/server/render-result.ts
+++ packages/next/src/server/render-result.ts
@@ -1,4 +1,5 @@
 import type { OutgoingHttpHeaders, ServerResponse } from 'http'
+import type { Readable } from 'node:stream'
 import type { CacheControl } from './lib/cache-control'
 import type { FetchMetrics } from './base-http'
 
@@ -8,7 +9,11 @@ import {
   streamFromString,
   streamToString,
 } from './stream-utils/node-web-streams-helper'
-import { isAbortError, pipeToNodeResponse } from './pipe-readable'
+import {
+  isAbortError,
+  pipeToNodeResponse,
+  pipeNodeReadableToNodeResponse,
+} from './pipe-readable'
 import type { RenderResumeDataCache } from './resume-data-cache/resume-data-cache'
 import { InvariantError } from '../shared/lib/invariant-error'
 import type {
@@ -73,6 +78,7 @@ export type RenderResultMetadata = AppPageRenderResultMetadata &
 export type RenderResultResponse =
   | ReadableStream<Uint8Array>[]
   | ReadableStream<Uint8Array>
+  | Readable
   | string
   | Buffer
   | null
@@ -83,6 +89,16 @@ export type RenderResultOptions<
   contentType: ContentTypeOption | null
   waitUntil?: Promise<unknown>
   metadata: Metadata
+}
+
+function isNodeReadable(value: unknown): value is Readable {
+  return (
+    value !== null &&
+    typeof value === 'object' &&
+    typeof (value as Record<string, unknown>).pipe === 'function' &&
+    typeof (value as Record<string, unknown>).on === 'function' &&
+    !(value instanceof ReadableStream)
+  )
 }
 
 export default class RenderResult<
@@ -222,6 +238,12 @@ export default class RenderResult<
       return chainStreams(...this.response)
     }
 
+    if (isNodeReadable(this.response)) {
+      const { Readable: NodeReadable } =
+        require('node:stream') as typeof import('node:stream')
+      return NodeReadable.toWeb(this.response) as ReadableStream<Uint8Array>
+    }
+
     return this.response
   }
 
@@ -244,6 +266,10 @@ export default class RenderResult<
       return this.response
     } else if (Buffer.isBuffer(this.response)) {
       return [streamFromBuffer(this.response)]
+    } else if (isNodeReadable(this.response)) {
+      const { Readable: NodeReadable } =
+        require('node:stream') as typeof import('node:stream')
+      return [NodeReadable.toWeb(this.response) as ReadableStream<Uint8Array>]
     } else {
       return [this.response]
     }
@@ -329,6 +355,16 @@ export default class RenderResult<
    * @param res
    */
   public async pipeToNodeResponse(res: ServerResponse) {
+    if (
+      this.response !== null &&
+      typeof this.response !== 'string' &&
+      !Buffer.isBuffer(this.response) &&
+      !Array.isArray(this.response) &&
+      isNodeReadable(this.response)
+    ) {
+      await pipeNodeReadableToNodeResponse(this.response, res, this.waitUntil)
+      return
+    }
     await pipeToNodeResponse(this.readable, res, this.waitUntil)
   }
 }
